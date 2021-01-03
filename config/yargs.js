7f901d0d43beab1925bbd857344b25758bb616d8
Felipe <fepipe82@hotmail.com>
2021-01-03 01:16:09 -0300

    Primer commit

---
+++ config/yargs.js
@@ -0,0 +1,23 @@
+const descripcion = {
+    demand: true,
+    alias: 'd',
+    desc: 'Descripción de la tarea por hacer'
+}
+
+const completado = {
+    alias: 'c',
+    default: true,
+    desc: 'Marca como completado o pendiente la tarea'
+}
+
+const argv = require('yargs')
+    .command('crear', 'Crear un elemento por hacer', { descripcion })
+    .command('actualizar', 'Actualiza el estado completado de una tarea', { descripcion, completado })
+    .command('listar', 'Mostrar todas las tareas por hacer', { completado })
+    .command('borrar', 'Elimina una tarea por hacer', { descripcion })
+    .help()
+    .argv;
+
+module.exports = {
+    argv
+}
---
+++ por-hacer/por-hacer.js
@@ -0,0 +1,87 @@
+const fs = require('fs');
+
+let listadoPorHacer = [];
+
+const guardarDB = () => {
+
+    let data = JSON.stringify(listadoPorHacer);
+
+    fs.writeFile('db/data.json', data, (err) => {
+        if (err) throw new Error('No se pudo grabar', err);
+    });
+
+}
+
+const cargarDB = () => {
+
+    try {
+        listadoPorHacer = require('../db/data.json');
+    } catch (error) {
+        listadoPorHacer = [];
+    }
+
+
+}
+
+const crear = (descripcion) => {
+
+    cargarDB();
+
+    let porHacer = {
+        descripcion,
+        completado: false
+    };
+
+    listadoPorHacer.push(porHacer);
+
+    guardarDB();
+
+    return porHacer;
+
+}
+
+const getListado = (completado) => {
+
+    cargarDB();
+    if (completado === true)
+        return listadoPorHacer.filter(tarea => tarea.completado === false);
+    else
+        return listadoPorHacer.filter(tarea => tarea.completado === true);
+}
+
+const actualizar = (descripcion, completado = true) => {
+
+    cargarDB();
+
+    let index = listadoPorHacer.findIndex(tarea => tarea.descripcion === descripcion);
+
+    if (index >= 0) {
+        listadoPorHacer[index].completado = completado;
+        guardarDB();
+        return true;
+    } else {
+        return false;
+    }
+
+}
+
+const borrar = (descripcion) => {
+    cargarDB();
+
+    let index = listadoPorHacer.findIndex(tarea => tarea.descripcion === descripcion);
+
+    if (index >= 0) {
+        listadoPorHacer.splice(index, 1);
+        guardarDB();
+        return true;
+    } else {
+        return false;
+    }
+}
+
+module.exports = {
+    crear,
+    getListado,
+    actualizar,
+    borrar
+}
